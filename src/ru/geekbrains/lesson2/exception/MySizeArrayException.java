996407d6a93d4997eb6b629dd27e16d11a09a545
duckpool <kulgachov88@gmail.com>
2021-03-14 13:24:27 +0300

    Lesson 2 Done!

---
+++ src/ru/geekbrains/lesson2/exception/MySizeArrayException.java
@@ -0,0 +1,12 @@
+package ru.geekbrains.lesson2.exception;
+
+public class MySizeArrayException extends RuntimeException{
+
+    public MySizeArrayException() {
+    }
+
+    public MySizeArrayException(String message) {
+        super(message);
+    }
+
+}
