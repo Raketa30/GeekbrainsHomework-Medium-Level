d94871a40ff09ea753c33eb1bd53b227d5a37d41
duckpool <kulgachov88@gmail.com>
2021-03-21 21:34:26 +0300

    lesson2

--- src/ru/geekbrains/lesson1/Main.java
+++ src/ru/geekbrains/lesson1/Main.java
@@ -1,4 +1,14 @@
 package ru.geekbrains.lesson1;
 
 public class Main {
+    public static void main(String[] args) throws InterruptedException {
+        Team team = new Team(4);
+        System.out.println(team.getTeam());
+
+        Course course = new Course(10);
+        System.out.println(course);
+        course.dolt(team);
+
+        System.out.println(team.getWinners());
+    }
 }
