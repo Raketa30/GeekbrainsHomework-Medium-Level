56f86da6ca7f4951a87d39b7d5e62bc9b8139c95
duckpool <kulgachov88@gmail.com>
2021-03-21 20:43:21 +0300

    Lesson 4 is Done

---
+++ src/ru/geekbrains/lesson4/MyChat/InputTextPanel.java
@@ -0,0 +1,40 @@
+package ru.geekbrains.lesson4.MyChat;
+
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.KeyAdapter;
+import java.awt.event.KeyEvent;
+
+public class InputTextPanel extends JPanel {
+
+    public InputTextPanel(JTextArea chatArea) {
+        JTextField textField = new JTextField();
+        JButton button = new JButton("Enter");
+
+        setLayout(new BorderLayout());
+        setPreferredSize(new Dimension(490, 40));
+
+        button.setPreferredSize(new Dimension(50, 40));
+
+        button.addActionListener(e -> {
+            if(!textField.getText().equals("")) {
+                chatArea.append(textField.getText() + "\n");
+                textField.setText("");
+            }
+        });
+
+        textField.addKeyListener(new KeyAdapter() {
+            @Override
+            public void keyTyped(KeyEvent e) {
+                if(e.getKeyChar() == '\n' && !textField.getText().equals("")) {
+                    chatArea.append(textField.getText() + "\n");
+                    textField.setText("");
+                }
+            }
+        });
+
+        add(textField,BorderLayout.CENTER);
+        add(button, BorderLayout.EAST);
+    }
+
+}
